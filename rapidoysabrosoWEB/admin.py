c300e1cb4cf1f02783e8b8671a2fdf43d592245e
GODLISA <101341298+GODLISA@users.noreply.github.com>
2024-10-04 19:29:32 -0300

    EL GRAN PLAN B FUNCIONA 10 DE 10

--- rapidoysabrosoWEB/admin.py
+++ rapidoysabrosoWEB/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Url, Producto, PageSelector
+
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ('url', 'last_scraped')
+    search_fields = ('url',)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'fuente_url')
+    search_fields = ('nombre', 'fuente_url__url')
+
+@admin.register(PageSelector)
+class PageSelectorAdmin(admin.ModelAdmin):
+    list_display = ('url', 'product_selector', 'price_selector', 'description_selector', 'image_selector')
+    search_fields = ('url__url',)
--- rapidoysabrosoWEB/models.py
+++ rapidoysabrosoWEB/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Url(models.Model):
+    url = models.URLField(unique=True)  # URL que vas a scrapear
+    last_scraped = models.DateTimeField(null=True, blank=True)  # Fecha de la última vez que fue scrapeada
+
+    def __str__(self):
+        return self.url
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)  # Nombre del producto
+    precio = models.CharField(max_length=50)  # Precio del producto, puedes ajustar el max_length según sea necesario
+    descripcion = models.TextField(null=True, blank=True)  # Descripción del producto
+    imagen_url = models.URLField(null=True, blank=True)  # URL de la imagen del producto
+    fuente_url = models.ForeignKey(Url, on_delete=models.CASCADE)  # Relación con la URL desde la que se extrajo
+
+    def __str__(self):
+        return self.nombre
+    
+class PageSelector(models.Model):
+    url = models.ForeignKey(Url, on_delete=models.CASCADE)  # Relación con la URL correspondiente
+    product_selector = models.CharField(max_length=255)  # Selector CSS o XPath para los productos
+    price_selector = models.CharField(max_length=255)  # Selector CSS o XPath para los precios
+    description_selector = models.CharField(max_length=255, null=True, blank=True)  # Selector para la descripción
+    image_selector = models.CharField(max_length=255, null=True, blank=True)  # Selector para la imagen
+
+    def __str__(self):
+        return f'Selectores para {self.url}'
+
